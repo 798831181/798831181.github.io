b5f0903fa13fc708bb2c51ab8b117bfa24405935
onion <onion@192.168.2.106>
2022-07-10 12:32:46 +0800

    optimize project

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -7,6 +7,29 @@ module.exports = {
       nav:[
         {text:'index', link:'/index'},
         {text:'about', link:'/about'}
-      ]
+      ],
+      sidebar:[
+        {
+            text:'quick-start',
+            collapsible: true,
+            items:[
+              {
+                text:'vitePress介绍', link:'/quick-start/'
+              }
+            ]
+        },
+        {
+          text:'java核心技术',
+          collapsible: true,
+          items:[
+            {
+              text:'介绍',
+              link:'/core-java/'
+            }
+          ]
+          
+        }
+        ]
+    
     }
   }
---
+++ docs/quick-start/index.md
@@ -0,0 +1,4 @@
+# Quick Start
+## vitePress使用介绍
+
+### 首页怎么定制
